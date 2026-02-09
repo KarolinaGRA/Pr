5390ada43a837455045fd7e8e9f72eeededed622
KarolinaGRA <karolinagrazynska@gmail.com>
2026-02-09 07:34:34 +0100

    Add files via upload

---
+++ Hello/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  // var myName = 'Karol';
+  // late final myName;
+  // String myName = 'Karol';
+  const myName = 'Karol';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+}
