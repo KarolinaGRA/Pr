bda730f9908c6f06fa30ba48b1bfe0632eb8d6f0
KarolinaGRA <karolinagrazynska@gmail.com>
2026-02-09 07:46:45 +0100

    Add files via upload

---
+++ async and await/12_async_await.dart
@@ -0,0 +1,18 @@
+void main() async {
+  print('Incio del programa');
+
+  try {
+    final value = await httpGet('https://fernando-herrera.com/cursos');
+    print(value);
+  } catch (err) {
+    print('Tenemos un error: $err');
+  }
+  print('Find del programa');
+}
+
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
+  throw 'Error en la petición';
+
+  // return 'Tenemos un valor de la petición';
+}
